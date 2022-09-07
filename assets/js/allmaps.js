42050478b15a3e2d1116113a6e25c1ec8753da1d
Jules Schoonman <jules.schoonman@gmail.com>
2022-09-07 14:20:13 +0200

    added jekyll

---
+++ assets/js/allmaps.js
@@ -0,0 +1,34 @@
+---
+---
+async function fetchJson (url) {
+    return fetch(url).then((response) => response.json())
+  }
+  
+  async function addGeorefAnnotationByUrl(warpedMapSource, url) {
+    const annotation = await fetchJson(url)
+    warpedMapSource.addGeorefAnnotation(annotation)
+  }
+  
+  async function initialize(WarpedMapLayer, WarpedMapSource) {
+    const warpedMapSource = new WarpedMapSource()
+    const warpedMapLayer = new WarpedMapLayer({
+      source: warpedMapSource
+    })
+  
+    const map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        }),
+        warpedMapLayer
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([12.4964, 41.9028]),
+        zoom: 15
+      })
+    })
+    {% assign annotations = site.static_files | where: "json", true %}
+    {% for annotation in annotations %}addGeorefAnnotationByUrl(warpedMapSource, '{{ annotation.path }}')
+    {% endfor %}
+  }
